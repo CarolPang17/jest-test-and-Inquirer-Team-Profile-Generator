5353c2f2901a24d0ce0bc922135ab403829bd67c
carolpang <carolpang17@gmail.com>
2022-03-05 12:39:32 -0800

    css created

--- src/page-template.js
+++ src/page-template.js
@@ -2,24 +2,33 @@
 var htmlContent = [];
 
 
-function generateTeam(data) {
+function generatehtml(data) {
    var numOfTeams = Object.keys(data)
    console.log(`numOfTeams : ${numOfTeams}`)
    for (var i = 0; i < numOfTeams.length ;i++){
-     var numOfManger = data[i].manager.length
-     console.log(`curret Team  : team ${i}`)
-     console.log(`curret numOfManger  : ${numOfManger}`)
-     for(var j = 0l j < numOfManger ; j++){
-       
-     }
-    htmlContent.push(JSON.stringify(data[i].manager))
+    var newTeamContainer = `<div class="aTeam team${i}"> <p>hi this is team ${i}</p> <div>${generateTeam(data,i)}</div></div>`
+    htmlContent.push(newTeamContainer)
+
+    //  var numOfManger = data[i].manager.length
+    //  console.log(`curret Team  : team ${i}`)
+    //  console.log(`curret numOfManger  : ${numOfManger}`)
+    //  for(var j = 0; j < numOfManger ; j++){
+    //   htmlContent.push(JSON.stringify(data[i].manager[j]))
+    //  }
    }
-  //  numOfManger = data[i].manager.length
-  // //  htmlContent.push(JSON.stringify(data[i].manager)
-  // //  for(var j = 0; j < numOfManger ;j++){
-  // //   htmlContent.push(JSON.stringify(data[i].manager) )
-  // //  }
-  // }
+
+
+   function generateTeam(data,i){
+    //  var numOfManger = data[i].manager.length
+    //  console.log(`curret Team  : team ${i}`)
+    //  console.log(`curret numOfManger  : ${numOfManger}`)
+    //  for(var j = 0; j < numOfManger ; j++){
+    //   htmlContent.push(JSON.stringify(data[i].manager[j]))
+    //  }
+
+    return 'try this work???'
+   }
+
   return htmlContent ;
 
   // return JSON.stringify(data)
@@ -35,10 +44,11 @@ function generateMarkdown(data) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
   </head>
   <body>
-  ${generateTeam(data)}
+  ${generatehtml(data)}
   </body>
   </html>
     `;
