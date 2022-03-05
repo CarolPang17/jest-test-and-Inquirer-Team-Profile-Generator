5d88c197f1e4aac2ef54b0751f000dbdfd592b14
carolpang <carolpang17@gmail.com>
2022-03-05 02:36:31 -0800

    team can be create with any type any numbers of emplyee

--- index.js
+++ index.js
@@ -1,66 +1,65 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const generatorMarkdown = require(path.join(__dirname, "/src/page-template.js"))
+const generatorMarkdown = require(path.join(
+  __dirname,
+  "/src/page-template.js"
+));
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-var allEmplyee ={}
+var allEmplyee = {};
 var teamNum = -1;
+var managerMenber;
 var engMenber;
 var internMenber;
 
 const questions = async () => {
-  const answers = await inquirer
-  .prompt([
-  {
-    type: "input",
-    message: "What the name of this emplyee?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is the ID number of this emplyee?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is the email of this emplyee?",
-    name: "email",
-  },
-  {
-    type: "list",
-    message: "What is the role of this emplyee?",
-    name: "role",
-    choices: ["Engineer", "Intern", "Manager"],
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What the name of this emplyee?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the ID number of this emplyee?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the email of this emplyee?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "What is the role of this emplyee?",
+      name: "role",
+      choices: ["Engineer", "Intern", "Manager"],
+    },
+  ]);
+  if (answers.role === "Manager") {
+    await addManger(answers);
   }
-])
-if(answers.role === "Manager"){
-  teamNum = teamNum + 1;
-  console.log(`teamNum : ${teamNum}`)
-  await addManger(answers)
-}
 
-if(answers.role === "Engineer"){
-  await addEngineer(answers)
-}
+  if (answers.role === "Engineer") {
+    await addEngineer(answers);
+  }
 
-if(answers.role === "Intern"){
-  await addIntern(answers)
-}
-
-}
+  if (answers.role === "Intern") {
+    await addIntern(answers);
+  }
+};
 
 const addManger = async (answers) => {
-  const mangerAnswers = await inquirer
-  .prompt([
+  const mangerAnswers = await inquirer.prompt([
     {
       type: "input",
       message: "please enter the office number of this Manager?",
-      name: "officeNumber"
-    }
-  ])
+      name: "officeNumber",
+    },
+  ]);
 
   const newManager = new Manager(
     answers.name,
@@ -69,43 +68,39 @@ const addManger = async (answers) => {
     mangerAnswers.officeNumber
   );
 
-  allEmplyee[teamNum] = {};
-  allEmplyee[teamNum].manager = newManager
-  engMenber = [];
-  internMenber = [];
+  allEmplyee[teamNum].manager = managerMenber;
+  managerMenber.push(newManager);
 
-return
-}
+  return;
+};
 
 const addEngineer = async (answers) => {
-  const enginnerAnswers = await inquirer
-  .prompt([
+  const enginnerAnswers = await inquirer.prompt([
     {
       type: "input",
       message: "please enter the GitHub user name for this Engineer?",
-      name: "github"
-    }
-  ])
+      name: "github",
+    },
+  ]);
   const newEngineer = new Engineer(
     answers.name,
     answers.id,
     answers.email,
     enginnerAnswers.github
   );
-  allEmplyee[teamNum].engineer = engMenber
-  engMenber.push(newEngineer)
-return
-}
+  allEmplyee[teamNum].engineer = engMenber;
+  engMenber.push(newEngineer);
+  return;
+};
 
 const addIntern = async (answers) => {
-  const internAnswers = await inquirer
-  .prompt([
+  const internAnswers = await inquirer.prompt([
     {
       type: "input",
       message: "Please enter the school of this intern attend?",
       name: "school",
-    }
-  ])
+    },
+  ]);
   const newIntern = new Intern(
     answers.name,
     answers.id,
@@ -113,12 +108,10 @@ const addIntern = async (answers) => {
     internAnswers.school
   );
 
-  allEmplyee[teamNum].intern = internMenber
-  internMenber.push(newIntern)
-  console.log(allEmplyee)
-return
-}
-
+  allEmplyee[teamNum].intern = internMenber;
+  internMenber.push(newIntern);
+  return;
+};
 
 function writeToFile(fileName, data) {
   return new Promise((resolve, reject) => {
@@ -136,21 +129,67 @@ function writeToFile(fileName, data) {
     });
   });
 }
+  // function printDataFun() {
+  //       const printData =  inquirer.prompt([
+  //     {
+  //       name: "wantPrintData",
+  //       type: "list",
+  //       choices: [
+  //         "Yes, please print all emplyee data as of now",
+  //         "No thanks , do not print now",
+  //       ],
+  //       message:
+  //         "Would you like to print the emplyee data that you just entered?",
+  //     },
+  //   ]);
 
+  //   if (
+  //     printData.wantPrintData === "Yes, please print all emplyee data as of now"
+  //   ) {
+  //     console.log(`-----------------------------`);
+  //     console.log(`below is current teams and emplyee data`);
+  //     console.log(JSON.stringify(allEmplyee));
+  //   }
+  // }
+  async function createNewTeam() {
+  teamNum = teamNum + 1;
+  console.log(`--------------------------------------------------`);
+  console.log(` We are now building a new team |  Team Number : ${teamNum} `);
+  console.log(`--------------------------------------------------`);
+
+  allEmplyee[teamNum] = {};
+  managerMenber = [];
+  engMenber = [];
+  internMenber = [];
+}
 
 async function init() {
-  await questions()
+  if (teamNum === -1) {
+    createNewTeam();
+  }
+  await questions();
 
-  await questions()
+  const newAction = await inquirer.prompt([
+    {
+      name: "action",
+      type: "list",
+      choices: ["Add a new member", "add new team", "Create Html"],
+      message: "What would you like to do next?",
+    },
+  ]);
+  if (newAction.action === "add new team") {
+    // if (teamNum >= 0) {
+    //  printDataFun()
+    // }
+    createNewTeam();
+    return init();
+  }
 
- await questions()
- await questions()
- await questions()
- await questions()
- await questions()
+  if (newAction.action === "Add a new member") {
+    return init();
+  }
 
-
-  writeToFile("newWeb.html", generatorMarkdown(allEmplyee))
+  return writeToFile("newWeb.html", generatorMarkdown(allEmplyee));
 }
 
 init();
